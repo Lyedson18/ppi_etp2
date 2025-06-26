cf131865cd1562e454e3d89bca35c24276c681a5
Lyedson_LEE <lyedsonmatheus13epic@gmail.com>
2025-06-26 16:52:08 +0000

    desafio4etp2

---
+++ src/components/etp2atvs/LuckyNumber.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import styles from "./LuckyNumber.module.css";
+export function LuckyNumber() {
+  const [luckyNumber, setLuckyNumber] = useState(1);
+  const [array, setArray] = useState([]);
+  function handleClick() {
+    setLuckyNumber(Math.floor(Math.random() * 50) + 1);
+    {array.includes(luckyNumber) ? (
+          console.log("Lucky number already exists in the array") 
+        ) : (
+          setArray([...array, luckyNumber])
+        )}
+  }
+  const formatedArray = array.join(", ");
+  return (
+    <div className={styles.container}>
+        {luckyNumber === 0 ? (
+          <h1>Lucky Number 🎲</h1>
+        ) : (
+          <h1>Lucky Number = {luckyNumber}</h1>
+        )}
+        {array.includes(luckyNumber) ? (
+          <h1> O número já foi sorteado</h1>
+        ) : (
+          console.log("Lucky number is unique")
+        )} 
+      <button className={styles.button} onClick={handleClick}>
+        you are lucky, try again!
+      </button>
+      <h2>Números sorteados:</h2>
+      <h2>{formatedArray}</h2>
+    </div>
+  );
+}
